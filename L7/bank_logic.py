ef4d866d0ca33c7dc43d9bc463beb536cc0bf542
anka <nanaladutska@gmail.com>
2026-08-05 17:32:14 +0300

    Add L7 homework

---
+++ L7/bank_logic.py
@@ -0,0 +1,14 @@
+def show():
+    print("Отображение списка счетов")
+def add():
+    print("Добавить счет")
+def delete():
+    print("Удалить счет")
+def change():
+    print("Изменить счет")
+    
+if __name__ == '__main__':
+    show()
+    add()
+    delete()
+    change()
---
+++ L7/main.py
@@ -0,0 +1,8 @@
+import bank_logic
+def main():
+    bank_logic.show()
+    bank_logic.add()
+    bank_logic.delete()
+    bank_logic.change()
+if __name__ == '__main__':
+    main()    
